8f8ec9bff54cec33616c36483632e514542c9236
Jack - KIJO <jack@kijo.co>
2020-05-27 17:26:38 +0100

    style form

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -23,7 +23,7 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   int _counter = 0;
 
-  void _incrementCounter() {
+  void _onSubmit() {
     setState(() {
       // This call to setState tells the Flutter framework that something has
       // changed in this State, which causes it to rerun the build method below
@@ -32,7 +32,15 @@ class _LoginPageState extends State<LoginPage> {
       // called again, and so nothing would appear to happen.
       _counter++;
     });
+
+    // Validate will return true if the form is valid, or false if
+    // the form is invalid.
+    if (_formKey.currentState.validate()) {
+      // Process data.
+    }
   }
+
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -49,35 +57,63 @@ class _LoginPageState extends State<LoginPage> {
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pshed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
+          // Center is a layout widget. It takes a single child and positions it
+          // in the middle of the parent.
+          child: Form(
+              key: _formKey,
+              child: Center(
+                widthFactor: 5,
+                child: Container(
+                  margin: const EdgeInsets.fromLTRB(10.0, 5.0, 10.0, 300.0),
+                  padding: const EdgeInsets.all(20.0),
+                  // color: Colors.amber[600],
+                  width: 500.0,
+                  height: 300.0,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          hintText: 'Enter your username',
+                        ),
+                        validator: (value) {
+                          if (value.isEmpty) {
+                            return 'Please enter your username';
+                          }
+                          return null;
+                        },
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 16.0),
+                        child: TextFormField(
+                          obscureText: true,
+                          decoration: const InputDecoration(
+                            hintText: 'Enter your password',
+                          ),
+                          validator: (value) {
+                            if (value.isEmpty) {
+                              return 'Please enter your password';
+                            }
+                            return null;
+                          },
+                        ),
+                      ),
+                      SizedBox(
+                          width: double.infinity,
+                          child: RaisedButton(
+                            onPressed: _onSubmit,
+                            child: Text('Login'),
+                          )),
+                      Padding(
+                          padding: EdgeInsets.fromLTRB(0, 30.0, 0, 0),
+                          child: Column(children: <Widget>[
+                            Text('User - user & password'),
+                            Text('Admin - admin & password')
+                          ]))
+                    ],
+                  ),
+                ),
+              ))),
       // floatingActionButton: FloatingActionButton(
       //   onPressed: _incrementCounter,
       //   tooltip: 'Increment',
