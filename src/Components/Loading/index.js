c22f150f78a860a9603d29c3f84ce9f7be5f22c8
Jack - KIJO <jack@kijo.co>
2020-05-28 12:00:27 +0100

    fix loader

--- src/Components/Loading/index.js
+++ src/Components/Loading/index.js
@@ -1,11 +1,10 @@
 import React, { Component } from "react";
-import logo from "../../spog.png";
 
 class Loading extends Component {
     render() {
         return (
             <div className="page_loading">
-                <img src={logo} className="App-logo" alt="logo" />
+                <div className="App-logo" alt="logo" />
             </div>
         );
     }
