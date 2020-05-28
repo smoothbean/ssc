3dc365682b50590fd014664c5618dd8f6674e74a
Jack - KIJO <jack@kijo.co>
2020-05-28 11:14:57 +0100

    Updates

---
+++ precache-manifest.8245ba312cf68742df847c4606b139f3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2855e1804061719bcc08729a2d698d01",
+    "url": "/scc/index.html"
+  },
+  {
+    "revision": "7aa09a38e843e6f77b3e",
+    "url": "/scc/static/css/main.9d50c557.chunk.css"
+  },
+  {
+    "revision": "af8e60aa0601fd3bd0d4",
+    "url": "/scc/static/js/2.bb47a19f.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/scc/static/js/2.bb47a19f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7aa09a38e843e6f77b3e",
+    "url": "/scc/static/js/main.d69fad9b.chunk.js"
+  },
+  {
+    "revision": "3c08bc67b42de58d437e",
+    "url": "/scc/static/js/runtime-main.2384ba4c.js"
+  },
+  {
+    "revision": "2ef8f0aed80b64675aab346d3c54382e",
+    "url": "/scc/static/media/spog.2ef8f0ae.png"
+  },
+  {
+    "revision": "1b1fe88e58bb073964e9c126790bebe2",
+    "url": "/scc/static/media/sweets.1b1fe88e.jpg"
+  }
+]);
